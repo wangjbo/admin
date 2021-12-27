e138838439b6fb3d5c4ddddbcddf4a749caee48a
wangjbo <708873100@qq.com>
2021-12-27 12:51:25 +0800

    merge

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,5 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-<<<<<<< HEAD
-import Login from './Login.js'
-import AdminIndex from './AdminIndex'
-
-function Main(props){
-    return (
-        <>
-            <Router>
-                <Route path='/' exact component={Login} />
-=======
 import Login from './Login'
 import AdminIndex from './AdminIndex'
 
@@ -18,7 +8,6 @@ function Main(){
         <>
             <Router>
                 <Route path='/' exact component={Login}></Route>
->>>>>>> c3219dd2210ad87ef7ec2180b7ec147c57fef36e
                 <Route path="/index/" exact component={AdminIndex} />
             </Router>
         </>
